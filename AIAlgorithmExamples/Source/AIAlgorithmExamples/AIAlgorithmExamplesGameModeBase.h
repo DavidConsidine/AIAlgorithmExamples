41bb6ee24d05f656c8eacaef1208382681e5c9e6
DavidConsidine <dmconsie@gmail.com>
2018-06-30 18:31:29 +0100

    1. Setup Unreal project.
    Create blank C++ Unreal project w/ starter content.

---
+++ AIAlgorithmExamples/Source/AIAlgorithmExamples/AIAlgorithmExamplesGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIAlgorithmExamplesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIALGORITHMEXAMPLES_API AAIAlgorithmExamplesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
